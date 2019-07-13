00294abddc873ef1688f2d3e7ffcd5d09f3d92b8
Saad E <21321555+se447@users.noreply.github.com>
2019-07-13 15:44:36 -0400

    Created constructor for BoundedCounter method

--- src/BoundedCounter.java
+++ src/BoundedCounter.java
@@ -1,3 +1,9 @@
 public class BoundedCounter {
-
+    private int value;
+    private int upperLimit;
+    
+    public BoundedCounter(int upperLimit) {
+        this.value = 0;
+        this.upperLimit = upperLimit;
+    }
 }
