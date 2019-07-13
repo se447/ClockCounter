c70770187ad4e35c016a5d22e6d722fe8c758875
Saad E <21321555+se447@users.noreply.github.com>
2019-07-13 15:59:31 -0400

    Updated toString method

--- src/BoundedCounter.java
+++ src/BoundedCounter.java
@@ -16,6 +16,10 @@ public class BoundedCounter {
     }
     
     public String toString() {
+        if (this.value < 10) {
+            return "0" +this.value;
+        }
+        
         return "" + this.value;
     }
 }
